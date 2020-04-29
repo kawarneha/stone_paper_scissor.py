d2d99f29cfcf9fd12a8a744ba621e3869c90cfa0
kawarneha <64544435+kawarneha@users.noreply.github.com>
2020-04-29 21:11:55 +0530

    Add files via upload

---
+++ stone_paper_scissor.py
@@ -0,0 +1,53 @@
+import random
+
+print("how many times u wanna play this game....?")
+
+x=int(input())
+
+for i in range(x):
+
+    comp=("scissor","stone","paper")
+
+    choice=random.choice(comp)
+
+    print("welcome:)")
+
+    print("enter your choice for the game-----stone,paper,scissor")
+
+    choice_u=input()
+
+    print("computer choice - ",end="")
+
+    print(choice,end="")
+
+    print(" therfore ",end="")
+
+    if(choice==choice_u):
+
+        print("match is draw")
+
+    elif(choice=="stone" and choice_u=="paper"):
+
+        print("you won:)")
+
+    elif(choice=="stone" and choice_u=="scissor"):
+
+        print("you lose;(")
+
+    elif(choice=="paper" and choice_u=="scissor"):
+ 
+        print("you won:)")
+
+    elif(choice=="paper" and choice_u=="stone"):
+
+        print("u lose;(")
+
+    elif(choice=="scissor" and choice_u=="stone"):
+
+        print("u won:)")
+
+    elif(choice=="scissor" and choice_u=="paper"):
+
+        print("u lose;(")
+
+    print("thanks for playing:]")
